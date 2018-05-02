4ba5df3ad6ed5a1802a87b425ed2f9b0b64121be
unknown <workinter01@workinter01.co.runexis.ru>
2018-05-02 19:50:17 +0500

    - avoid 502 error

--- cmd/publish_process.js
+++ cmd/publish_process.js
@@ -7,8 +7,13 @@ module.exports = async (path, ...args) => {
         utils.service._client.setToken(cfg.token);
         let wp = await utils.service.workplaces.get(cfg.workplace);
         let process = await wp.processes.get(cfg.processId);
+        let start = true
 
-        await process.stop();
+        await process.stop().catch(err => {
+            if (err.error_code === 502) {
+                start = false;
+            }
+        })
 
         let conns = await utils.walkConnectors(path);
         for (let i = 0; i < conns.length; i++) {
@@ -18,9 +23,10 @@ module.exports = async (path, ...args) => {
             await process.updateConnector(conns[i].id, conns[i]);
         }
 
-        await process.start();
-        console.log('Done.');
-        
+        if (start)
+            await process.start();
+        console.log('Done.')
+
     } catch (err) {
         utils.error(err);
     }
